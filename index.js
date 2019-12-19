6e066ec69748aaf8ac5e026e55be7424487c54c8
Elias Jørgensen <eliaszakarias@gmail.com>
2019-12-19 14:28:28 +0100

    Create version spell action

---
+++ index.js
@@ -0,0 +1,17 @@
+const core = require('@actions/core')
+const { execSync } = require('child_process')
+
+const theSpell = 'echo $GITHUB_REF \\\n' +
+	'| sed \'s/refs\\/heads\\///\' \\\n' +
+	'| sed \'s/master/dev/\' \\\n' +
+	'| sed \'s/feature\\///\' \\\n' +
+	'| sed \'s/release\\///\' \\\n' +
+	'| sed \'s/$/.\'"$(git rev-list --no-merges --count $GITHUB_REF).$(git rev-parse --short $GITHUB_REF)"\'/\''
+
+try {
+	const result = execSync(theSpell)
+	console.log(result.toString())
+	// core.setOutput('version', result)
+} catch (error) {
+	// core.setFailed(error.message)
+}
