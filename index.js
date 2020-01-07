5e6937b1179b06fda25c57675ae2b17635e3f3c4
Mikkel Løkke <ml@acto.dk>
2020-01-07 16:16:19 +0100

    Possible fix for getting short hash.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const theSpell = 'echo `git branch | grep \\* | cut -d \' \' -f2` \\\n' +
 	'| sed \'s/master/dev/\' \\\n' +
 	'| sed \'s/feature\\///\' \\\n' +
 	'| sed \'s/release\\///\' \\\n' +
-	'| sed \'s/$/.\'"$(git rev-list --no-merges --count `git branch | grep \\* | cut -d \' \' -f2`).$(git rev-parse --short `git branch | grep \\* | cut -d \' \' -f2`)"\'/\''
+	'| sed \'s/$/.\'"$(git rev-list --no-merges --count `git branch | grep \\* | cut -d \' \' -f2`).$(git describe --always)"\'/\''
 
 try {
 	const result = execSync(theSpell)
