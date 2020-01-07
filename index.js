14fde3d3a380549db4701bfc85782a9f4ffb234f
Mikkel Løkke <ml@acto.dk>
2020-01-07 17:42:02 +0100

    Never mind.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const core = require('@actions/core')
 const { execSync } = require('child_process')
 
-const theSpell = 'echo `git branch --show-current` \\\n' +
+const theSpell = 'echo $GITHUB_REF \\\n' +
 	'| sed \'s/master/dev/\' \\\n' +
 	'| sed \'s/feature\\///\' \\\n' +
 	'| sed \'s/release\\///\' \\\n' +
-	'| sed \'s/$/.\'"$(git rev-list --no-merges --count $GITHUB_REF).$(git describe --always)"\'/\''
+	'| sed \'s/$/.\'"$(git rev-list --no-merges --count $GITHUB_REF).$(git describe --always $GITHUB_REF)"\'/\''
 
 try {
 	const result = execSync(theSpell)
