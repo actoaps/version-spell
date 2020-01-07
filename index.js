7f3fdedf084457308a43f811a41b0510e80d4cdc
Mikkel Løkke <ml@acto.dk>
2020-01-07 15:56:53 +0100

    Github agnostic spell. Hopefully.

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const core = require('@actions/core')
 const { execSync } = require('child_process')
 
-const theSpell = 'echo $GITHUB_REF \\\n' +
-	'| sed \'s/refs\\/heads\\///\' \\\n' +
+const theSpell = 'echo `git branch | grep \\* | cut -d \' \' -f2` \\\n' +
 	'| sed \'s/master/dev/\' \\\n' +
 	'| sed \'s/feature\\///\' \\\n' +
 	'| sed \'s/release\\///\' \\\n' +
-	'| sed \'s/$/.\'"$(git rev-list --no-merges --count $GITHUB_REF).$(git rev-parse --short $GITHUB_REF)"\'/\''
+	'| sed \'s/$/.\'"$(git rev-list --no-merges --count `git branch | grep \\* | cut -d \' \' -f2`).$(git rev-parse --short `git branch | grep \\* | cut -d \' \' -f2`)"\'/\''
 
 try {
 	const result = execSync(theSpell)
