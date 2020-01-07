3596e637088d21d6723be2ae6199b8f0168d1465
Mikkel Løkke <ml@acto.dk>
2020-01-07 16:42:10 +0100

    Sigh.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const core = require('@actions/core')
 const { execSync } = require('child_process')
 
-const theSpell = 'echo `git branch | grep \\* | cut -d \' \' -f2` \\\n' +
+const theSpell = 'echo `git rev-parse --abbrev-ref HEAD` \\\n' +
 	'| sed \'s/master/dev/\' \\\n' +
 	'| sed \'s/feature\\///\' \\\n' +
 	'| sed \'s/release\\///\' \\\n' +
-	'| sed \'s/$/.\'"$(git rev-list --no-merges --count `git branch | grep \\* | cut -d \' \' -f2`).$(git describe --always)"\'/\''
+	'| sed \'s/$/.\'"$(git rev-list --no-merges --count `git rev-parse --abbrev-ref HEAD`).$(git describe --always)"\'/\''
 
 try {
 	const result = execSync(theSpell)
