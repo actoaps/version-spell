796ae7b8980f11331b28ce9f55ae5709bda4feba
Omar A. Hachach <contact@omarhachach.com>
2022-11-11 16:16:02 +0100

    Add main and fix release versioning

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
 const core = require('@actions/core')
-const { execSync } = require('child_process')
+const {execSync} = require('child_process')
 
 const commitCountCommand = 'git rev-list --count HEAD'
 const descriptionHashCommand = 'git describe --always'
 const branchCommand = 'git rev-parse --abbrev-ref HEAD'
 
 try {
-	const commitCount = execSync(commitCountCommand).toString().trim()
-	const descriptionHash = execSync(descriptionHashCommand).toString().trim()
-	const branch = execSync(branchCommand).toString().trim()
+    const commitCount = execSync(commitCountCommand).toString().trim()
+    const descriptionHash = execSync(descriptionHashCommand).toString().trim()
+    let branch = execSync(branchCommand).toString().trim()
 
-	if (branch == null || branch === '') {
-		core.setFailed('Could not extract branch name')
-		return
-	}
+    if (branch == null || branch === '') {
+        core.setFailed('Could not extract branch name')
+        return
+    }
 
-	let version
+    if (branch === 'master' || branch === 'main') {
+        branch = 'dev'
+    }
 
-	const num = branch.replace('release/', '')
-		.replace('test/', '')
-	if (num === '') {
-		version = `${num}.${commitCount}-${descriptionHash}`
-	} else {
-		const tag = branch
-			.replace('master', 'dev')
-			.replace(/[/\\?%*:|"<>]/g, '-')
-		version = `${tag}.${commitCount}-${descriptionHash}`
-	}
+    const num = branch.replace('release/', '')
+        .replace('test/', '')
+        .replace(/[/\\?%*:|"<>]/g, '-')
 
-	core.info('Version is: ' + version)
-	core.setOutput('version', version)
+    const version = `${num}.${commitCount}-${descriptionHash}`
+
+    core.info('Version is: ' + version)
+    core.setOutput('version', version)
 } catch (error) {
-	core.setFailed(error.message)
+    core.setFailed(error.message)
 }
